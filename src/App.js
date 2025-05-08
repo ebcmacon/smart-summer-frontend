43df89c86dd578280821e77a8cd6c45a80be8f73
Carl <ebcmacon@gmail.com>
2025-05-08 05:00:19 -0500

    Add Reading Lesson 2 link and route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import ReadingLesson from "./ReadingLesson";
+import ReadingSentenceMatch from "./ReadingSentenceMatch"; // 🔹 new import
 
 function App() {
   return (
@@ -9,9 +10,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/reading" element={<ReadingLesson />} />
+        <Route path="/reading2" element={<ReadingSentenceMatch />} /> {/* 🔹 new route */}
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
--- src/Home.js
+++ src/Home.js
@@ -5,9 +5,16 @@ export default function Home() {
   return (
     <div style={{ padding: 20 }}>
       <h1>Welcome to Smart Summer!</h1>
-      <Link to="/reading">
-        <button>Start Reading Lesson</button>
-      </Link>
+
+      <div style={{ marginTop: 20 }}>
+        <Link to="/reading">
+          <button style={{ marginRight: 10 }}>📖 Reading Lesson 1</button>
+        </Link>
+
+        <Link to="/reading2">
+          <button>🧠 Reading Lesson 2</button>
+        </Link>
+      </div>
     </div>
   );
 }

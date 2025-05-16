ccb10338cc222f85a6e0da1adf99ed351b45d909
ebcmacon <ebcmacon@gmail.com>
2025-05-15 21:37:17 -0500

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,13 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
-    <div style={{ padding: 20 }}>
-      <h1>Welcome to Smart Summer!</h1>
-
-      <div style={{ marginTop: 20 }}>
-        <Link to="/reading">
-          <button style={{ marginRight: 10 }}>📖 Reading Lesson 1</button>
-        </Link>
-
-        <Link to="/reading2">
-          <button>🧠 Reading Lesson 2</button>
-        </Link>
-      </div>
+    <div style={{ padding: 40 }}>
+      <h1>Smart Summer Dashboard</h1>
+      <p>
+        <Link to="/login">🔑 Go to Admin Login</Link>
+      </p>
     </div>
   );
 }
-

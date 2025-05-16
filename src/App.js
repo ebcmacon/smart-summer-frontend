1e8de023b5493946d355fa8f5f494a8dab30218d
ebcmacon <ebcmacon@gmail.com>
2025-05-15 21:32:11 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
-import Login from './Login';            // <- correct!
+import Login from './Login';
 import AdminDashboard from './AdminDashboard';
 
 function App() {
   const [token, setToken] = useState(localStorage.getItem('token'));
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/"    element={<Home />} />
         <Route path="/login" element={<Login setToken={setToken} />} />
         <Route path="/admin" element={<AdminDashboard />} />
       </Routes>
